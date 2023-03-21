08f4a916a63cc8e5761e2cba5c1178d5e2f710ab
Murilo Kauan Socek <socekmurilo@gmail.com>
2023-03-21 09:47:48 -0300

    main

---
+++ main.py
@@ -0,0 +1,15 @@
+import Murilo
+
+print("Soma")
+a = int(input("Digite o 1° valor: "))
+b = int(input("Digite o 2° valor: "))
+
+soma = Murilo.soma(a, b)
+print(soma)
+
+print("Subtração")
+a = int(input("Digite o 1° valor: "))
+b = int(input("Digite o 2° valor: "))
+
+sub = Murilo.sub(a, b)
+print(sub)
