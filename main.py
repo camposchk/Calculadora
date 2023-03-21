873f374c0597d546d13e2823942dce80010c6289
Murilo Kauan Socek <socekmurilo@gmail.com>
2023-03-21 10:46:29 -0300

    Erros

--- main.py
+++ main.py
@@ -2,21 +2,37 @@ import Murilo
 
 print("Você deseja fazer uma Soma ou Subtração?")
 print("\n1 - Somar \n2 - Subtrair\n")
+while True:
+  try:
+      resp = int(input("Digite qual vc deseja: "))
+      if resp < 3 and resp > 0:
+        break
+  except ValueError:
+    print("Valor Invalido!")
+    print()
 
-resp = int(input("Digite qual vc deseja: "))
 
 if resp == 1:
   print("Soma")
-  a = int(input("Digite o 1° valor: "))
-  b = int(input("Digite o 2° valor: "))
+  while True:
+    try:
+      a = int(input("Digite o 1° valor: "))
+      b = int(input("Digite o 2° valor: "))
+      break
+    except ValueError:
+      print("Valor Invalido")
 
   soma = Murilo.soma(a, b)
   print(soma)
 
 elif resp == 2:
   print("Subtração")
-  a = int(input("Digite o 1° valor: "))
-  b = int(input("Digite o 2° valor: "))
-
+  while True:
+    try:
+        a = int(input("Digite o 1° valor: "))
+        b = int(input("Digite o 2° valor: "))
+        break
+    except ValueError:
+      print("Valor Invalido")
   sub = Murilo.sub(a, b)
   print(sub)
